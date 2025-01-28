bc351ce45176509ff99ec15177556427ac310544
rokugin <rokugin@gmail.com>
2025-01-27 20:01:38 -0700

    Add console commands to print needed recipes to console

--- ModEntry.cs
+++ ModEntry.cs
@@ -16,6 +16,8 @@ internal class ModEntry : Mod {
         helper.Events.Content.AssetRequested += static (_, e) => AssetManager.OnAssetRequested(e);
         helper.Events.Content.AssetsInvalidated += static (_, e) => AssetManager.OnAssetInvalidated(e);
 
+        helper.ConsoleCommands.Add("rokugin.exclusions", "Print list of recipes needed for perfection.", PrintExclusions);
+
         var harmony = new Harmony(MODID);
 
         harmony.Patch(
@@ -26,4 +28,73 @@ internal class ModEntry : Mod {
             postfix: new HarmonyMethod(typeof(UtilityPatch), nameof(UtilityPatch.GetCraftedRecipesPercent)));
     }
 
+    private void PrintExclusions(string command, string[] args) {
+        if (!Context.IsWorldReady) return;
+
+        if (args.Length < 1) {
+            Monitor.Log("\nrokugin.exclusions command missing arguments.\nExpected values:\ncooking\ncrafting\n", LogLevel.Error);
+            return;
+        }
+
+        if (args[0] == "cooking") {
+            Monitor.Log(BuildRecipeList(args[0]), LogLevel.Info);
+            return;
+        }
+
+        if (args[0] == "crafting") {
+            Monitor.Log(BuildRecipeList(args[0]), LogLevel.Info);
+            return;
+        }
+
+        Monitor.Log($"{args[0]} not recognized as a valid argument.\nExpected values:\ncooking\ncrafting\n", LogLevel.Error);
+    }
+
+    string BuildRecipeList(string recipeTypes) {
+        string s = "\n";
+        List<string> recipesList = new();
+
+        if (recipeTypes == "cooking") {
+            foreach (var group in AssetManager.Recipes) {
+                if (group.Value.CookingRecipes != null) recipesList.AddRange(group.Value.CookingRecipes);
+            }
+
+            Dictionary<string, string> recipes = new Dictionary<string, string>(CraftingRecipe.cookingRecipes);
+
+            foreach (var v in recipes) {
+                string recipeKey = v.Key;
+
+                if (recipesList.Contains(recipeKey)) {
+                    continue;
+                }
+
+                string recipe = ArgUtility.SplitBySpaceAndGet(ArgUtility.Get(v.Value.Split('/'), 2), 0);
+
+                if (!Game1.player.recipesCooked.ContainsKey(recipe)) {
+                    s += $"{recipeKey}\n";
+                }
+            }
+        } else {
+            foreach (var group in AssetManager.Recipes) {
+                if (group.Value.CraftingRecipes != null) recipesList.AddRange(group.Value.CraftingRecipes);
+            }
+
+            Dictionary<string, string> recipes = new Dictionary<string, string>(CraftingRecipe.craftingRecipes);
+
+            foreach (var v in recipes) {
+                string recipeKey = v.Key;
+
+                if (recipeKey == "Wedding Ring" || recipesList.Contains(recipeKey)) {
+                    continue;
+                }
+
+                if (Game1.player.craftingRecipes.TryGetValue(recipeKey, out var timesCrafted) && timesCrafted > 0) {
+                    continue;
+                }
+
+                s += $"{recipeKey}\n";
+            }
+        }
+
+        return s;
+    }
 }
